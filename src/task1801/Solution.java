47b55e1087b27be0045e00ee94d28807e2eaa489
Artem <aptemkaoas123@gmail.com>
2021-12-26 13:56:23 +0300

    exemple IO

---
+++ src/task1801/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Максимальный байт
+Ввести с консоли имя файла.
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода.
+
+
+Requirements:
+1. Программа должна считывать имя файла с консоли.
+2. Для чтения из файла используй поток FileInputStream.
+3. В консоль должен выводиться максимальный байт, считанный из файла.
+4. Поток чтения из файла должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        FileInputStream stream = new FileInputStream(fileName);
+        List<Integer> result = new ArrayList<>();
+        while (stream.available() > 0) {
+            int data = stream.read();
+            result.add(data);
+        }
+        stream.close();
+        Collections.sort(result);
+        System.out.println(result.get(result.size() - 1));
+    }
+}
