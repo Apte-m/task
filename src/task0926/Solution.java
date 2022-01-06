0eb3ba715670c71243dd671492caf064d211f90b
Artem <aptemkaoas123@gmail.com>
2022-01-06 14:43:56 +0300

    convert

---
+++ src/task0926/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task09.task0926;
+
+
+import java.util.ArrayList;
+
+/* 
+Список из массивов чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] one = new int[5];
+        int[] two = new int[2];
+        int[] three = new int[4];
+        int[] four = new int[7];
+        int[] five = new int[0];
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        list.add(four);
+        list.add(five);
+
+        return list;
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            for (int x : array) {
+                System.out.println(x);
+            }
+        }
+    }
+}
