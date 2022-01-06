4161928134d083d34dbacc53a5a5b22b1bf07ac4
Artem <aptemkaoas123@gmail.com>
2022-01-07 01:12:22 +0300

    NOD method Efklid

---
+++ src/task1420/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+НОД
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scannerOne = new Scanner(System.in);
+        Scanner scannerTwo = new Scanner(System.in);
+        int a = scannerOne.nextInt();
+        int b = scannerTwo.nextInt();
+
+        System.out.println(GCD(a,b));
+
+    }
+
+    public static int GCD ( int a, int b){
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+}
