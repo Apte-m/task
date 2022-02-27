802144acab1c025a13feecd8b9d1e9c28aa66c64
Artem <aptemkaoas123@gmail.com>
2022-02-27 11:39:22 +0300

    arcanoid

---
+++ src/task2413/Canvas.java
@@ -0,0 +1,39 @@
+package task2413;
+
+public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+    void setPoint(double x, double y, char c){}
+    void drawMatrix(double x, double y, int[][] matrix, char c){}
+}
