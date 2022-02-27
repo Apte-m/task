10892691860298ae49b5d604ca3a9f6bc0115aea
Artem <aptemkaoas123@gmail.com>
2022-02-27 18:22:49 +0300

    marker

---
+++ src/task2401/SelfInterfaceMarker.java
@@ -0,0 +1,5 @@
+package task2401;
+
+public interface SelfInterfaceMarker {
+
+}
---
+++ src/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+
+
+
+    public void returnInteger() {
+
+    }
+
+    public void returnToString() {
+
+    }
+}
